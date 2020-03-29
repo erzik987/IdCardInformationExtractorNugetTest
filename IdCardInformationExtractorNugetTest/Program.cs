78d210ff67b8c632bd8b8216d7c316b6ffb2e676
Erik Hudcovský <erik.hudcovsky@solarwinds.com>
2020-03-29 07:59:01 +0200

    init

---
+++ IdCardInformationExtractorNugetTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdCardInformationExtractorNugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
